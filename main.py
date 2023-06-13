2b4c1b627ef7e32db8a8cb5f157f5e2cce0ecbae
Robu Petru <petru.r.robu@gmail.com>
2023-06-13 13:20:50 +0300

    Mai multe capmuri

--- main.py
+++ main.py
@@ -39,34 +39,57 @@ class Gui:
         self.frame = tk.Frame(self.window)
         self.frame.pack()
 
-        self.data_frame = tk.LabelFrame(self.frame, text = "Informatii: ")
+        self.frame_CI = tk.LabelFrame(self.frame, text = "Informatii CI: ")
+        self.frame_Loc = tk.LabelFrame(self.frame, text = "Informatii adresa:")
 
-        self.label_nume = tk.Label(self.data_frame, text = 'Nume: ')
-        self.label_prenume = tk.Label(self.data_frame, text = 'Prenume: ')
-        self.label_cnp = tk.Label(self.data_frame, text = 'CNP: ')
+        self.labels = [tk.Label(self.frame_CI, text = 'Nume: '), 
+            tk.Label(self.frame_CI, text = 'Prenume: '),
+            tk.Label(self.frame_CI, text = 'CNP: '),
+            tk.Label(self.frame_Loc, text = 'Strada: '),
+            tk.Label(self.frame_Loc, text = 'Nr: '),
+            tk.Label(self.frame_Loc, text = 'Tara: '),
+            tk.Label(self.frame_Loc, text = 'Localitate: '),
+            tk.Label(self.frame_Loc, text = 'Sector: '),
+        ]
 
-        self.box_nume = tk.Entry(self.data_frame)
-        self.box_prenume = tk.Entry(self.data_frame)
-        self.box_cnp = tk.Entry(self.data_frame)
+        self.boxes = [tk.Entry(self.frame_CI),
+            tk.Entry(self.frame_CI),
+            tk.Entry(self.frame_CI),
+            tk.Entry(self.frame_Loc),
+            tk.Entry(self.frame_Loc),
+            tk.Entry(self.frame_Loc),
+            tk.Entry(self.frame_Loc),
+            tk.Entry(self.frame_Loc)
+        ]
 
     def collect_data(self):
-        prenume = self.box_prenume.get()
-        nume = self.box_nume.get()
-        cnp = self.box_cnp.get()
-        
+        prenume = self.boxes[1].get()
+        nume = self.boxes[0].get()
+        cnp = self.boxes[2].get()
+        std = self.boxes[3].get()
+        nr = self.boxes[4].get()
+        tara = self.boxes[5].get()
+        loc = self.boxes[6].get()
+        sect = self.boxes[7].get()
+
         if not prenume.isalpha():
             tk.messagebox.showwarning(title='Atentie!', message='Prenume invalid!')
-            self.box_prenume.delete(0, tk.END)
+            self.boxes[1].delete(0, tk.END)
         elif not nume.isalpha():
             tk.messagebox.showwarning(title='Atentie!', message='Nume invalid!')
-            self.box_nume.delete(0, tk.END)
+            self.boxes[0].delete(0, tk.END)
         elif not len(cnp) == 13 or not cnp.isnumeric():
             tk.messagebox.showwarning(title='Atentie!', message='CNP invalid!')
-            self.box_cnp.delete(0, tk.END)
+            self.boxes[2].delete(0, tk.END)
         else:
             self.collected_data['nume'] = nume
             self.collected_data['prenume'] = prenume
             self.collected_data['cnp'] = cnp
+            self.collected_data['strada'] = std
+            self.collected_data['nr'] = nr
+            self.collected_data['tara'] = tara
+            self.collected_data['localitate'] = loc
+            self.collected_data['sector'] = sect
             tk.messagebox.showinfo(title='Receptionat!', message='Date Introduse Corect!')
             self.window.quit()
 
@@ -74,19 +97,37 @@ class Gui:
     def params(self):
         self.window.title("Formular Introducere Date")
         
-        self.data_frame.grid(row=0, column=0, padx=20, pady=20)
-        self.label_nume.grid(row=0, column=0)
-        self.label_prenume.grid(row=0, column=1)
-        self.label_cnp.grid(row=2, column=0)
-        self.box_nume.grid(row=1, column=0)
-        self.box_prenume.grid(row=1, column=1)
-        self.box_cnp.grid(row=3, column=0)
+        self.frame_CI.grid(row=0, column=0, padx=20, pady=20)
+        self.frame_Loc.grid(row=1, column=0, padx=20, pady=20)
 
-        for widget in self.data_frame.winfo_children():
+        self.labels[0].grid(row=0, column=0)
+        self.labels[1].grid(row=0, column=1)
+        self.labels[2].grid(row=2, column=0)
+
+        self.labels[3].grid(row=0, column=0)
+        self.labels[4].grid(row=0, column=1)
+        self.labels[5].grid(row=0, column=2)
+        self.labels[6].grid(row=0, column=3)
+        self.labels[7].grid(row=2, column=0)
+
+        self.boxes[0].grid(row=1, column=0)
+        self.boxes[1].grid(row=1, column=1)
+        self.boxes[2].grid(row=3, column=0)
+
+        self.boxes[3].grid(row=1, column=0)
+        self.boxes[4].grid(row=1, column=1)
+        self.boxes[5].grid(row=1, column=2)
+        self.boxes[6].grid(row=1, column=3)
+        self.boxes[7].grid(row=3, column=0)
+
+        for widget in self.frame_CI.winfo_children():
+            widget.grid_configure(padx=10, pady=5)
+
+        for widget in self.frame_Loc.winfo_children():
             widget.grid_configure(padx=10, pady=5)
 
         button = tk.Button(self.frame, text="Trimite", command=self.collect_data)
-        button.grid(row=1, column=0)
+        button.grid(row=4, column=0)
         button.grid_configure(padx=20, pady=10)
         
     def get_collected_data(self):
@@ -100,6 +141,7 @@ if __name__ == '__main__':
     gui = Gui()
     gui.start()
     info = gui.get_collected_data()
+    print(info)
 
     for filename in os.listdir(input_path):
         pdf = input_path + filename
